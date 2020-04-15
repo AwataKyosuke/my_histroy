615f1375fc1baaea8ea7195e5a97c99a54777cd4
Kyosuke <kyou0209@gmail.com>
2020-04-15 19:33:39 +0900

    vue-js-modalの導入

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VModal from 'vue-js-modal'
 import state from '@/store/state'
 import actions from '@/store/actions'
 import getters from '@/store/getters'
 import mutations from '@/store/mutations'
 
 Vue.use(Vuex)
+Vue.use(VModal)
 
 export default new Vuex.Store({
   state,
