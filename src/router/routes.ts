19b481e4a6963607361d23c4c897fbd2863db077
Kyosuke <kyou0209@gmail.com>
2020-04-17 21:44:34 +0900

    REMOVE:不要なタイトル属性の削除

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,24 +8,20 @@ export default [
     path: '/',
     name: 'Home',
     component: Home,
-    meta: { title: 'ホーム' }
   },
   {
     path: '/articles/:id',
     name: 'Article',
     component: Article,
-    meta: { title: '備忘録' }
   },
   {
     path: '/articles/new',
     name: 'NewArticle',
     component: NewArticle,
-    meta: { title: '新規投稿' }
   },
   {
     path: '*',
     name: 'NotFound',
     component: NotFound,
-    mata: { title: '404' }
   }
 ]
