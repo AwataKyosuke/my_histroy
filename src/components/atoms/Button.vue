f2bfdfecb0231c1d1483e4c4d0c106ecaecf90cb
Kyosuke <kyou0209@gmail.com>
2020-04-18 14:13:29 +0900

    ボタンデザイン変更

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -20,17 +20,19 @@ export default {
   display: inline-block;
   padding: 0.4em 1.6em;
   font-size: 0.8em;
-  color: #00b5ad;
+  color: #fff;
   text-decoration: none;
   user-select: none;
+  background: #00b5ad;
   border: 1px #00b5ad solid;
   border-radius: 3px;
-  transition: 0.4s ease;
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 2px 0 rgba(0, 0, 0, 0.12);
+  transition: 0.2s ease;
 }
 
 .button:hover {
-  color: #fff;
-  background: #00b5ad;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2), 0 1px 8px 0 rgba(0, 0, 0, 0.12);
+  cursor: pointer;
 }
 
 </style>
